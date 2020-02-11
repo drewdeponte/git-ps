7184216bbface1e8d01c518cb935af2b980122f8
Andrew De Ponte <cyphactor@gmail.com>
2020-02-11 10:32:57 -0800

    Set supported platform to macOS v10.13
    I did this because I wanted to use the Process API methods that were
    introduced in 10.13.
    
    ps-id: 36519294-405F-4D3E-938C-F9A9051C0697

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "git-ps",
+    platforms: [
+        SupportedPlatform.macOS(SupportedPlatform.MacOSVersion.v10_13)
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
