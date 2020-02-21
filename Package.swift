0d2924f7004c5899f1e6d27f2805c2776c61a2d2
Andrew De Ponte <cyphactor@gmail.com>
2020-02-20 22:19:49 -0800

    Add JsonFileManager v2.0.0 as a dependency
    So that I can use it to implement the storage facility for review
    request records in git-ps.

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "git@github.com:uptech/JsonFileManager.git", Package.Dependency.Requirement.upToNextMajor(from: Version(2, 0, 0)))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -20,7 +21,7 @@ let package = Package(
             dependencies: ["GitPatchStackCore"]),
         .target(
             name: "GitPatchStackCore",
-            dependencies: []),
+            dependencies: ["JsonFileManager"]),
         .testTarget(
             name: "git-psTests",
             dependencies: ["git-ps"]),
