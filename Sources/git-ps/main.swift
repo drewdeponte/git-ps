d8dee220644201dc3e32b518208ffd4916645beb
ChillBroDev <jon@upte.ch>
2020-03-02 10:33:05 -0800

    Add help message when InvalidArgumentCount Exception is thrown.
    Help message shows valid commands.
    
    ps-id: BF5B7C18-F51A-4B09-B889-886D5062D9AC

--- Sources/git-ps/main.swift
+++ Sources/git-ps/main.swift
@@ -4,6 +4,9 @@ let gitPatchStack = try! GitPatchStack()
 
 do {
     try gitPatchStack.run()
+} catch GitPatchStack.Error.invalidArgumentCount {
+    print("Default commands are: \n\n git-ps (ls, show <patch-index>, pull, rebase, rr <patch-index>, pub <patch-index>, --version).")
 } catch {
     print("Whoops! An error occurred: \(error)")
 }
+
